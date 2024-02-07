44f4f33b74052b509dea2f78895cab40439ac1df
asmauzatin <144237776+asmauzatin@users.noreply.github.com>
2024-02-07 06:59:15 -0800

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,1 +1,12 @@
+(function(window){
+    var helloSpeaker = {}; // Step 3: Create an object called 'helloSpeaker'
+    
+    var speakWord = "Hello"; // Step 2: Do not attach speakWord to helloSpeaker object
+    
+    helloSpeaker.speak = function(name) { // Step 4: Attach the 'speak' method to helloSpeaker object
+      console.log(speakWord + " " + name);
+    }
+    
+    window.helloSpeaker = helloSpeaker; // Step 5: Expose the 'helloSpeaker' object to the global scope
+})(window);
 
