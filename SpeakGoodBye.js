3be6420d0d9e607b974699e648fcdbd98a22129e
asmauzatin <144237776+asmauzatin@users.noreply.github.com>
2024-02-07 06:55:32 -0800

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,1 +1,12 @@
+(function(window){
+    var byeSpeaker = {}; // Step 7: Create an object called 'byeSpeaker'
+
+    var speakWord = "Good Bye"; // Step 6: Do not attach speakWord to byeSpeaker object
+    
+    byeSpeaker.speak = function(name) { // Step 8: Attach the 'speak' method to byeSpeaker object
+      console.log(speakWord + " " + name);
+    }
+    
+    window.byeSpeaker = byeSpeaker; // Expose the 'byeSpeaker' object to the global scope
+})(window);
 
